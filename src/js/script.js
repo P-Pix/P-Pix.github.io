7cc20b326fe0bdf2b68ceb19c049bd06824a3733
Guillaume-L <guillaume200202@gmail.com>
2025-07-22 10:50:49 +0200

    init

--- src/js/script.js
+++ src/js/script.js
@@ -9,18 +9,20 @@ anchor.addEventListener("click", function (e) {
 });
 
 const phrases = [
-"Informaticien IA & Santé",
-"Spécialiste Data Science biomédicale",
-"Passionné par l'innovation médicale"
+  "Informaticien en intelligence artificielle et sciences des données,<br>avec une spécialisation appliquée à la santé",
+  "Titulaire d'un Master en Informatique,<br>spécialisé en Data Science et Intelligence Artificielle",
+  "Maîtrise des outils de modélisation IA,<br>analyse de données biomédicales et développement web",
+  "Curiosité scientifique, rigueur technique,<br>engagement associatif fort et un sens de l'humain"
 ];
+
 let i = 0;
 
 setInterval(() => {
-const el = document.querySelector(".hero p");
-el.style.opacity = 0;
-setTimeout(() => {
-    el.textContent = phrases[i % phrases.length];
+  const el = document.querySelector(".hero p");
+  el.style.opacity = 0;
+  setTimeout(() => {
+    el.innerHTML = phrases[i % phrases.length];  // utiliser innerHTML pour interpréter les <br>
     el.style.opacity = 1;
     i++;
-}, 500);
-}, 3000);
+  }, 500);
+},5000);
